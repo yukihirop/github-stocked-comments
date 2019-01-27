8b48fa9d6928f75ec109b2eabb0f59e50c61e600
yukihirop <te108186@gmail.com>
2019-01-27 18:01:30 +0900

    content_scriptが上手く動いた。

--- src/manifest.js
+++ src/manifest.js
@@ -10,40 +10,33 @@ module.exports = {
     '16': 'icons/16.png',
     '128': 'icons/128.png'
   },
-  /**
-   * @see {@link https://developer.chrome.com/extensions/declare_permissions}
-   */
-  permissions: [
-    '<all_urls>',
-    '*://*/*',
-    'activeTab',
-    'tabs',
-    'background',
-    'unlimitedStorage',
-    'storage'
-  ],
   browser_action: {
     default_title: 'title',
     default_popup: 'pages/popup.html'
   },
-  background: {
-    persistent: false,
-    page: 'pages/background.html'
-  },
-  options_page: 'pages/options.html',
+  /**
+   * @see {@link https://developer.chrome.com/extensions/declare_permissions}
+   */
+  permissions: [
+    // '<all_urls>',
+    // '*://*/*',
+    'activeTab',
+    'tabs'
+  ],
   content_scripts: [{
-    js: [
-      'js/manifest.js',
-      'js/vendor.js',
-      'js/content.js'
+    matches: [
+      "https://github.com/",
+      "https://github.com/*"
     ],
-    run_at: 'document_end',
-    matches: ['<all_urls>'],
-    all_frames: true
+    js: [
+      "js/manifest.js",
+      "js/vendor.js",
+      "js/content.js"
+    ]
   }],
   manifest_version: 2,
   content_security_policy: "script-src 'self'; object-src 'self'",
   web_accessible_resources: [
-    'js/content.js'
+    "js/content.js"
   ]
 }
