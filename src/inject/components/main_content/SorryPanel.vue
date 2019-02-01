15e7e302e0cc51e3ca5b5f5073268c9bc4fc54e7
yukihirop <te108186@gmail.com>
2019-02-01 10:24:37 +0900

    Implement SorryPanel

---
+++ src/inject/components/main_content/SorryPanel.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="blankslate">
+    <h3>Sorry, we couldn’t find any matching comments.</h3>
+    <p>You might find what you’re looking for by 
+      <a data-pjax="true" href="/search?q=aaa">searching all of GitHub</a>for
+      <strong>“aaa”</strong>.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SorryPanel',
+  props: {
+    href: {
+      type: String,
+      required: true,
+      default: ''
+    },
+    searchWord: {
+      type: String,
+      required: true,
+      default: ''
+    }
+  }
+}
+</script>
