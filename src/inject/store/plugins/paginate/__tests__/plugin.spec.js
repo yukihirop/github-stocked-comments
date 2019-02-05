c61cd48a37ddd13e648bcb63758926d339c1957c
yukihirop <te108186@gmail.com>
2019-02-05 14:43:30 +0900

    Add callback Paginate plugin

--- src/inject/store/plugins/paginate/__tests__/plugin.spec.js
+++ src/inject/store/plugins/paginate/__tests__/plugin.spec.js
@@ -1,28 +1,70 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { mapState, mapActions } from 'vuex'
 import plugin, { Paginate } from '../index.js'
 
 Vue.use(Vuex)
 
 function getStoreWithPlugin(plugins){
   const initialState = {
-    currentCommentData: []
+    currentCommentData: [],
+    count: 0
   }
-
+  
   return new Vuex.Store({
+    root: true,
     state: initialState,
+    actions: {
+      countup: store => store.commit('countup')
+    },
+    mutations: {
+      countup: state => state.count++
+    },
     plugins
   })
 }
 
 describe('plugin', () => {
-  it('should instantiate Paginate and accessible through store.paginate', () => {
+  describe('initialize', () => {
+    it('should instantiate Paginate and accessible through store.paginate', () => {
+      const plugins = [ plugin({ resourceName: 'currentCommentData', perPage: 5 }) ]
+      const store = getStoreWithPlugin(plugins)
+
+      expect(store.paginate instanceof Paginate).toBe(true)
+    })
+  })
+
+  describe('callback', () => {
+    const mockPaginate_Next = jest.fn()
+    let testPaginate
+
     const plugins = [
-      plugin({ resourceName: 'currentCommentData', perPage: 5 })
+      plugin({ resourceName: 'currentCommentData', perPage: 5 }, (store, paginate) => {
+        store.subscribe((mutation, sate) => {
+          if (mutation.type === 'countup') {
+            mockPaginate_Next()
+            testPaginate = paginate
+          }
+        })
+      })
     ]
 
     const store = getStoreWithPlugin(plugins)
+    const vm = new Vue({
+      store,
+      computed: {
+        ...mapState(['count'])
+      },
+      methods: {
+        ...mapActions(['countup'])
+      }
+    })
 
-    expect(store.paginate instanceof Paginate).toBe(true)
+    it('countup and page move to next', () => {
+      vm.countup()
+      expect(vm.count).toEqual(1)
+      expect(testPaginate instanceof Paginate).toBe(true)
+      expect(mockPaginate_Next).toBeCalledTimes(1)
+    })
   })
 })
--- src/inject/store/plugins/paginate/plugin.js
+++ src/inject/store/plugins/paginate/plugin.js
@@ -3,9 +3,10 @@ import Paginate from './plugin/Paginate'
 export default function plugin ({
   resourceName,
   perPage = 5
-} = {}) {
+} = {}, callback = () => {}) {
   return (store) => {
     // eslint-disable-next-line no-new
-    new Paginate({ store, resourceName, perPage })
+    const paginate = new Paginate({ store, resourceName, perPage })
+    callback(store, paginate)
   }
 }
