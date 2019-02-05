0073f8113e329dcd6eff5e2b9c828b4bfb877520
yukihirop <te108186@gmail.com>
2019-02-05 23:32:34 +0900

    Implement SortOptions

--- src/inject/components/github/main_content/SelectDetailsMenu.vue
+++ src/inject/components/github/main_content/SelectDetailsMenu.vue
@@ -2,38 +2,53 @@
   <details class="details-reset details-overlay select-menu position-relative">
     <summary class="btn select-menu-button" aria-haspopup="menu">
       <i>Sort:</i>
-      <span data-menu-button="">Recently comments </span>
+      <span data-menu-button="">{{ selectOptionName }}</span>
     </summary>
     <div class="select-menu-modal position-absolute right-0" style="z-index: 99" role="menu">
       <div class="select-menu-header">
         <span class="select-menu-title">Sort options</span>
       </div>
       <div class="select-menu-list">
-        <a role="menuitem" aria-checked="true" class="select-menu-item selected" href="https://github.com/stars?direction=desc&amp;sort=created" data-pjax="">
-          <svg class="octicon octicon-check select-menu-item-icon" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true">
-            <path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path>
-          </svg>
-          <span class="select-menu-item-text" data-menu-button-text="">Recently comments</span>
-        </a>
-        <a role="menuitem" aria-checked="false" class="select-menu-item " href="https://github.com/stars?direction=desc&amp;sort=updated" data-pjax="">
-          <svg class="octicon octicon-check select-menu-item-icon" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true">
-            <path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path>
-          </svg>
-          <span class="select-menu-item-text" data-menu-button-text="">Recently active</span>
-        </a>
-        <a role="menuitem" aria-checked="false" class="select-menu-item " href="https://github.com/stars?direction=desc&amp;sort=stars" data-pjax="">
-          <svg class="octicon octicon-check select-menu-item-icon" viewBox="0 0 12 16" version="1.1" width="12" height="16" aria-hidden="true">
-            <path fill-rule="evenodd" d="M12 5l-8 8-4-4 1.5-1.5L4 10l6.5-6.5L12 5z"></path>
-          </svg>
-          <span class="select-menu-item-text" data-menu-button-text="">Most stars</span>
-        </a>
+        <sort-option :selected-class="isSortRecentlyComments ? 'selected' : ''" :option-name="'Recently comments'" :sort-func="sortRecentlyComments" />
+        <sort-option :selected-class="isSortLongestBodyComments ? 'selected' : ''" :option-name="'Longest comments'" :sort-func="sortLongestBodyComments" />
       </div>
     </div>
   </details>
 </template>
 
 <script>
+import SortOption from '@/inject/components/github/main_content/select_details_menu/SortOption'
+import { mapActions } from 'vuex'
+
 export default {
-  name: 'SelectDetailsMenu'
+  name: 'SelectDetailsMenu',
+  components: {
+    SortOption: SortOption
+  },
+  data(){
+    return {
+      isSortRecentlyComments: true,
+      isSortLongestBodyComments: false,
+      selectOptionName: "Recently comments"
+    }
+  },
+  methods: {
+    ...mapActions({
+      _sortRecentlyComments: 'sortRecentlyComments',
+      _sortLongestBodyComments: 'sortLongestBodyComments'
+    }),
+    sortRecentlyComments(){
+      this._sortRecentlyComments()
+      this.isSortRecentlyComments = true
+      this.isSortLongestBodyComments = false
+      this.selectOptionName = "Recently comments"
+    },
+    sortLongestBodyComments(){
+      this._sortLongestBodyComments()
+      this.isSortRecentlyComments = false
+      this.isSortLongestBodyComments = true
+      this.selectOptionName = "Longest comments"
+    },
+  }
 }
 </script>
--- src/inject/store/actions.js
+++ src/inject/store/actions.js
@@ -16,5 +16,21 @@ export default {
       return comment.body.toLowerCase().includes(text.toLowerCase())
     })
     commit(types.SEARCH_COMMENT_DATA, { data: data, searchText: text })
+  },
+  sortRecentlyComments({ commit, state }) {
+    let sortData = state.commentData.sort((a, b) => {
+      if(a.createdAt < b.createdAt) return 1
+      if(a.createdAt > b.createdAt) return -1
+      return 0
+    })
+    commit(types.FETCH_COMMENT_DATA, { data: sortData })
+  },
+  sortLongestBodyComments({ commit, state}) {
+    let sortData = state.commentData.sort((a, b) => {
+      if(a.body.length < b.body.length) return 1
+      if(a.body.length > b.body.length) return -1
+      return 0
+    })
+    commit(types.FETCH_COMMENT_DATA, { data: sortData })
   }
 }
