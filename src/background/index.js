9a79b5bb5a316709968eaac646d5a55ac8bdb3e0
yukihirop <te108186@gmail.com>
2019-02-17 23:12:36 +0900

    WIP

--- src/background/index.js
+++ src/background/index.js
@@ -1,3 +1,5 @@
+let beforeURL = ''
+
 /* eslint-disable no-undef */
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === 'executeInjectScript') {
@@ -14,3 +16,37 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   }
 })
 /* eslint-enable no-undef */
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  try{
+    if (changeInfo.url) {
+      let isLoadStockedComments = false
+      let isOpenURL = false
+
+      console.log(changeInfo)
+      console.log(tab)
+      console.log(beforeURL)
+
+      if(changeInfo.url.indexOf("https://github.com") === -1) return
+      if(changeInfo.status === 'loading' && tab.url.indexOf("#Stocked_Comments") > 0) {
+        isLoadStockedComments = true
+        chrome.tabs.sendMessage(tabId, { isLoadStockedComments: isLoadStockedComments, isOpenURL: isOpenURL, openURL: tab.url }, (response) => {
+          console.log(response)
+        });
+      } else if (changeInfo.status === 'loading' && beforeURL.indexOf('#Stocked_Comments') > 0) {
+        isOpenURL = true
+        chrome.tabs.sendMessage(tabId, { isLoadStockedComments: isLoadStockedComments, isOpenURL: isOpenURL, openURL: tab.url }, (response) => {
+          console.log(response)
+        });
+      } else {
+        console.log("その他")
+        console.log(tabId)
+        console.log(changeInfo)
+      }
+      beforeURL = tab.url
+    }
+  } catch(e) {
+    console.log("エラーだ")
+    console.log(e)
+  }
+});
--- src/content/event.js
+++ src/content/event.js
@@ -2,14 +2,6 @@ import Header from '@/content/github/components/Header'
 import IssueComment from '@/content/github/components/IssueComment'
 import githubInjection from 'github-injection'
 
-export var addOnLoadListener = () => {
-  window.addEventListener('load', (event) => {
-    if (window.location.hash === '#Stocked_Comments') {
-      history.pushState('', document.title, window.location.pathname)
-    }
-  })
-}
-
 export var addGitHubInjection = () => {
   githubInjection(() => {
     if(new RegExp("issues").test(window.location.pathname)) {
@@ -27,20 +19,45 @@ export var addStockedCommentsClickListener = () => {
   })
 }
 
+// export var addHashChangeListener = () => {
+//   window.onhashchange = (event) => {
+//     var urlHash = window.location.hash
+//     if (urlHash === '#Stocked_Comments') {
+//       var mainContent = '<div id="github-stocked-comments"></div>'
+
+//       $('.application-main').empty()
+//       $('.application-main').append(mainContent)
+
+//       chrome.runtime.sendMessage({message: 'executeInjectScript'}, function (response) {
+//         console.log(response.farewell)
+//       })
+//     }
+//   }
+// }
+
 export var addHashChangeListener = () => {
-  window.onhashchange = (event) => {
-    var urlHash = window.location.hash
-    if (urlHash === '#Stocked_Comments') {
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    let isLoadStockedComments = request.isLoadStockedComments
+    let isOpenURL = request.isOpenURL
+    let openURL = request.openURL
+    if (isLoadStockedComments){
       var mainContent = '<div id="github-stocked-comments"></div>'
 
       $('.application-main').empty()
       $('.application-main').append(mainContent)
 
+      sendResponse({farewell: "call executeInjectScript (background)"})
       chrome.runtime.sendMessage({message: 'executeInjectScript'}, function (response) {
         console.log(response.farewell)
       })
+    } else if (isOpenURL) {
+      console.log("リロード")
+      sendResponse({farewell: "Reload browser"})
+      window.location.href = request.openURL
+    } else {
+      sendResponse({farewell: "Nothing"})
     }
-  }
+  });
 }
 
 export var addBrowserReloadListener = () => {
@@ -52,6 +69,14 @@ export var addBrowserReloadListener = () => {
       }
     }
   }, false)
+}
+
+export var addOnLoadListener = () => {
+  window.addEventListener('load', (event) => {
+    if (window.location.hash === '#Stocked_Comments') {
+      history.pushState('', document.title, window.location.pathname)
+    }
+  })
 }
 
 var pressCtrlorCommand = (e) => {
